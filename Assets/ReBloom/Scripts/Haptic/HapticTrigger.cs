157918cdfd76fda7530492a9d1867a786d3bbfcc
Sincer1ty <woodsy0417@gmail.com>
2026-05-06 03:18:26 +0900

    [Add] Simple Haptic Test

---
+++ Assets/ReBloom/Scripts/Haptic/HapticTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HapticTrigger : MonoBehaviour
+{
+    public XRNode targetNode = XRNode.RightHand;
+
+    // amplitude : 진동 세기, duration : 초
+    public void TriggerHaptic(float amplitude, float duration)
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(targetNode);
+
+        if (device.TryGetHapticCapabilities(out HapticCapabilities cap))
+        {
+            if (cap.supportsImpulse)
+            {
+                device.SendHapticImpulse(0, amplitude, duration);
+            }
+        }
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter");
+        TriggerHaptic(1.0f, 0.1f);
+    }
+}
