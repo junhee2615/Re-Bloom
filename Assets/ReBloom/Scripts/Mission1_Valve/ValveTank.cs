752253bcaf36226fd3de7130b3f572fde251bdda
agnes0827 <agnes20827@gmail.com>
2026-05-12 22:52:52 +0900

    밸브 미션

--- Assets/ReBloom/Scripts/Mission1_Valve/ValveTank.cs
+++ Assets/ReBloom/Scripts/Mission1_Valve/ValveTank.cs
@@ -2,13 +2,38 @@ using UnityEngine;
 
 public class ValveTank : MonoBehaviour
 {
+    public ValveMissionManager missionManager;
+
+    private float timer = 0f;
+
     private void OnTriggerStay(Collider other)
     {
         ValveHapticVib haptic = other.GetComponent<ValveHapticVib>();
 
         if (haptic != null)
         {
-            haptic.TriggerHaptic(1.0f, 0.1f);
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                float stability = missionManager.stability;
+
+                // stability ³·À»¼ö·Ï ·£´ý °­ÇÔ
+                float amplitude =
+                    Mathf.Lerp(Random.Range(0.2f, 1f),
+                               0.5f,
+                               stability);
+
+                // stability ³·À»¼ö·Ï ·£´ý °£°Ý
+                float duration =
+                    Mathf.Lerp(Random.Range(0.05f, 0.3f),
+                               0.15f,
+                               stability);
+
+                haptic.TriggerHaptic(amplitude, duration);
+
+                timer = duration;
+            }
         }
     }
 }
