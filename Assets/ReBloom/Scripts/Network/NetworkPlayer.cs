767e07d779d26cb2e378543e2a2b628e695c0b5d
agnes0827 <agnes20827@gmail.com>
2026-06-02 13:48:49 +0900

    networkplayer

--- Assets/ReBloom/Scripts/Network/NetworkPlayer.cs
+++ Assets/ReBloom/Scripts/Network/NetworkPlayer.cs
@@ -8,12 +8,22 @@ public class NetworkPlayer : NetworkBehaviour
     public GameObject locomotion;
     public UnityEngine.Behaviour[] xrComponents;
 
+    public Transform headTarget;
+    public Transform leftHandIKTarget;
+    public Transform rightHandIKTarget;
+
+    public Transform mainCameraTransform;
+    public Transform leftControllerTransform;
+    public Transform rightControllerTransform;
+
+
     public override void Spawned()
     {
         if (Object.HasInputAuthority)
         {
-            // 내 플레이어 
+            // 내 플레이어
             SetLayerRecursively(bodyRoot, LayerMask.NameToLayer("MyThirdPerson"));
+
             mainCamera.SetActive(true);
             locomotion.SetActive(true);
         }
@@ -21,13 +31,33 @@ public class NetworkPlayer : NetworkBehaviour
         {
             // 상대 플레이어
             SetLayerRecursively(bodyRoot, LayerMask.NameToLayer("ThirdPerson"));
+
             mainCamera.SetActive(false);
             locomotion.SetActive(false);
+
             foreach (var comp in xrComponents)
             {
                 comp.enabled = false;
             }
         }
+    }
+
+    private void LateUpdate()
+    {
+        if (!Object.HasInputAuthority)
+            return;
+
+        // 머리
+        headTarget.position = mainCameraTransform.position;
+        headTarget.rotation = mainCameraTransform.rotation;
+
+        // 왼손
+        leftHandIKTarget.position = leftControllerTransform.position;
+        leftHandIKTarget.rotation = leftControllerTransform.rotation;
+
+        // 오른손
+        rightHandIKTarget.position = rightControllerTransform.position;
+        rightHandIKTarget.rotation = rightControllerTransform.rotation;
     }
 
     void SetLayerRecursively(GameObject obj, int layer)
