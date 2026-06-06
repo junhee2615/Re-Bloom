fe9308478973d9d23e731eb4d538c8198d302f39
agnes0827 <agnes20827@gmail.com>
2026-06-06 22:58:22 +0900

    문 튀는 문제 해결

--- Assets/ReBloom/Scripts/Mission1_Valve/DoorRotate.cs
+++ Assets/ReBloom/Scripts/Mission1_Valve/DoorRotate.cs
@@ -1,15 +1,18 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using Fusion;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class DoorRotate : MonoBehaviour
+public class DoorRotate : NetworkBehaviour
 {
+    [Networked]
+    public float CurrentAngle { get; set; }
+
     private XRGrabInteractable grabInteractable;
     private Transform interactor;
 
     private float previousZ;
-    private float currentAngle;
 
     public float rotateDirection = 1f;
     public float rotateSpeed = 200f;
@@ -17,42 +20,53 @@ public class DoorRotate : MonoBehaviour
     public float minAngle = -90f;
     public float maxAngle = 90f;
 
-    void Start()
+
+    public override void Spawned()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
 
         grabInteractable.selectEntered.AddListener(OnGrab);
         grabInteractable.selectExited.AddListener(OnRelease);
 
-        currentAngle = transform.localEulerAngles.y;
-        if (currentAngle > 180f) 
-            currentAngle -= 360f;
+        float startAngle = transform.localEulerAngles.y;
+        if (startAngle > 180f)
+            startAngle -= 360f;
+
+        if (HasStateAuthority)
+            CurrentAngle = startAngle;
     }
 
-    void Update()
+    public override void FixedUpdateNetwork()
     {
-        if (interactor == null)
-            return;
+        Debug.Log($"Fixed / State:{HasStateAuthority}, Interactor:{interactor != null}, Angle:{CurrentAngle}");
+        if (HasStateAuthority && interactor != null)
+        {
+            float currentZ = interactor.position.z;
+            float delta = currentZ - previousZ;
 
-        float currentZ = interactor.position.z;
-        float delta = currentZ - previousZ;
+            // 한 프레임에 너무 크게 변하는 값 제한
+            delta = Mathf.Clamp(delta, -0.03f, 0.03f);
 
-        // 한 프레임에 너무 크게 변하는 값 제한
-        delta = Mathf.Clamp(delta, -0.03f, 0.03f);
+            CurrentAngle += -delta * rotateSpeed * rotateDirection;
+            CurrentAngle = Mathf.Clamp(CurrentAngle, minAngle, maxAngle);
 
-        currentAngle += -delta * rotateSpeed * rotateDirection;
-        currentAngle = Mathf.Clamp(currentAngle, minAngle, maxAngle);
-
-        transform.localRotation = Quaternion.Euler(0f, currentAngle, 0f);
-
-        previousZ = currentZ;
+            previousZ = currentZ;
+        }
+        transform.localRotation = Quaternion.Euler(0f, CurrentAngle, 0f);
     }
 
     private void OnGrab(SelectEnterEventArgs args)
     {
         Debug.Log("Grabbed Door");
+        
         interactor = args.interactorObject.transform;
         previousZ = interactor.position.z;
+
+        if (Object != null && !HasStateAuthority)
+        {
+            Object.RequestStateAuthority();
+            Debug.Log("StateAuthority 요청함");
+        }
     }
 
     private void OnRelease(SelectExitEventArgs args)
