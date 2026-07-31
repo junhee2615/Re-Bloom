5c315ad50b926dd8c6c2eab938584cfb94d8b258
smartrhj <smartrhj@gmail.com>
2026-08-01 01:02:24 +0900

    캐릭터 무릎 굽힘 기능 오류 수정
    - HMD 높이 초기화 문제 수정
    - 숙일 때 무릎이 정상적으로 굽혀지도록 개선
    - 굽힘 강도 조절 값 추가

--- Assets/ReBloom/Character_Art/Scripts/VRHandController.cs
+++ Assets/ReBloom/Character_Art/Scripts/VRHandController.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // 유니티 6에서 입력을 받기 위해 필수
+using UnityEngine.InputSystem;
 
 public class VRHandController : MonoBehaviour
 {
-    public Animator handAnimator;       // 손 모델의 애니메이터
-    public InputActionProperty gripAction; // 컨트롤러의 그립(잡기) 버튼 액션
-    public string parameterName = "Grip";  // 애니메이터에 만든 파라미터 이름
+    public Animator handAnimator;
+    public InputActionProperty gripAction;
+    public string parameterName = "Grip";
 
     void Update()
     {
-        // 1. 컨트롤러 버튼이 얼마나 눌렸는지 소수점 값(0.0~1.0)으로 읽어옵니다.
         float gripValue = gripAction.action.ReadValue<float>();
 
-        // 2. 그 값을 애니메이터의 "Grip" 파라미터에 넣어줍니다.
         handAnimator.SetFloat(parameterName, gripValue);
     }
 }
